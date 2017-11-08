df348394fbda561ccec151206dedd5724b3b15d1
Nofar Ben David <nofarbendavid@gmail.com>
2017-11-09 00:18:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React                    from 'react';
 import ReactDOM                 from 'react-dom';
 import App                      from './App';
-//import registerServiceWorker    from './registerServiceWorker';
 import { Provider }             from 'react-redux';
 import { createStore }          from 'redux';
 import rapidApp                 from './reducers'
@@ -18,8 +17,4 @@ ReactDOM.render(
     </Provider>
     ,
     document.getElementById('root'));
-
-// registerServiceWorker();
-
-
 
